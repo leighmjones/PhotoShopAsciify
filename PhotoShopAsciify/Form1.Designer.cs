463cb21bf8f8d5d96de463b8fca586f0284a9228
jonesleigh92 <jonesleigh92@gmail.com>
2018-08-23 21:51:13 -0500

    Add project files.

---
+++ PhotoShopAsciify/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace PhotoShopAsciify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.yBox = new System.Windows.Forms.NumericUpDown();
+            this.xBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.yBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(154, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(326, 421);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(154, 553);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(326, 417);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PapayaWhip;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(244, 1001);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ascify";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(2271, 42);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 38);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(173, 38);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(268, 38);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            this.openFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFile_FileOk);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 4.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(717, 85);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1542, 1602);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 1215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(239, 1263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Y";
+            // 
+            // yBox
+            // 
+            this.yBox.Location = new System.Drawing.Point(268, 1261);
+            this.yBox.Name = "yBox";
+            this.yBox.Size = new System.Drawing.Size(120, 31);
+            this.yBox.TabIndex = 12;
+            this.yBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // xBox
+            // 
+            this.xBox.Location = new System.Drawing.Point(268, 1215);
+            this.xBox.Name = "xBox";
+            this.xBox.Size = new System.Drawing.Size(120, 31);
+            this.xBox.TabIndex = 13;
+            this.xBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(2271, 1699);
+            this.Controls.Add(this.xBox);
+            this.Controls.Add(this.yBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Asciify";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.yBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown yBox;
+        private System.Windows.Forms.NumericUpDown xBox;
+    }
+}
+
---
+++ PhotoShopAsciify/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace PhotoShopAsciify
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        Bitmap bmp;
+        double normalized;
+        //byte [] read;
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFile.ShowDialog();
+        }
+
+        private void openFile_FileOk(object sender, CancelEventArgs e)
+        {
+            bmp = new Bitmap(openFile.FileName);
+
+            StreamReader file = new StreamReader(openFile.FileName);
+
+            file.ReadLine();
+            file.Close();
+
+            pictureBox1.Image = new Bitmap(openFile.FileName);
+
+
+            Color c;
+            int result;
+
+            //Greyscale image to second picture box
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+
+                    //Grab pixel values and store in C
+                    c = bmp.GetPixel(x, y);
+
+                    //Assign color values
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    result = (r + g + b) / 3;
+
+                    
+                    //Use average to greyscale the pixels and create a new image
+                    bmp.SetPixel(x, y, Color.FromArgb(255, result, result, result));
+
+                    
+                }
+            }
+            //Store greyscale image in the second picture box
+            pictureBox2.Image = bmp;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Asciitize Method to show ascii art
+            Asciitize(bmp);
+
+            //Store greyscale image in the second picture box
+            //pictureBox2.Image = bmp;
+        }
+
+        //Asciitize Method reads the bmp, converts to gray scale, and creates ascii art
+        public void Asciitize (Bitmap bmp)
+        {
+            //Builds the ascii art string
+            StringBuilder asciitext = new StringBuilder();
+
+            //Holds the normalized value
+            double kernel;
+
+            int xSTART = 0;
+            int ySTART = 0;
+
+            //Size
+            int xS = Convert.ToInt32(xBox.Value);
+            int yS = Convert.ToInt32(xBox.Value);
+
+            int columns = bmp.Height / yS;
+            int rows = bmp.Width / xS;
+            
+            //Loops through the bitmap & (x, y)
+            for (int y = 0; y < columns; y++)
+            {
+                for (int x = 0; x < rows; x++)
+                {
+                    //Calls kernel method
+                    kernel = AverageKernel(xSTART, ySTART, xS, yS, bmp);
+
+                    //Appends ascii characters by using the GrayToString Method
+                    asciitext.Append(GreyToString(kernel));
+
+                    xSTART += xS;
+                }
+
+                xSTART = 0;
+                ySTART += yS;
+                asciitext.Append("\r\n");
+            }
+
+            //Show ascii art in textbox
+            richTextBox1.Text = asciitext.ToString();
+
+        }
+
+        //Average Method returns the normalized value (0-1)
+        public double AverageColor (Color c)
+        {
+            //Assign color values
+            int a = c.A;
+            int r = c.R;
+            int g = c.G;
+            int b = c.B;
+
+            int avg = (r + g + b) / 3;
+            normalized = avg / 255.0;
+
+            //GreyToString(normalized);
+
+            return normalized;
+        }
+
+
+        public double AverageKernel (int xstart, int ystart, int xSize, int ySize, Bitmap bmp)
+        {
+            //Color variable
+            Color c;
+
+           
+            double norm;
+            double runTotal = 0.0;
+         
+
+
+            //Loops through the bitmap & (x, y)
+            for (int j = 0; j < ySize; j++)
+            {
+                for (int i = 0; i < xSize; i++)
+                {
+                    //Grab pixel values and store in C
+                    c = bmp.GetPixel(xstart, ystart);
+
+                    //Assign color values
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    int result = (r + g + b) / 3;
+
+                    norm = AverageColor(c);
+
+                    //Running total
+                    runTotal += norm;
+
+                    xstart++;
+                }
+
+                xstart -= xSize;
+                ystart ++;
+            }
+
+            
+            return runTotal / (xSize * ySize);
+        }
+
+        //Reads the normalized value and returns the associated string
+        public string GreyToString(double value)
+        {
+            string ascii = "";
+
+            if (value >= 0.90)
+            {
+                ascii = " ";
+            }
+            else if(value >= 0.80)
+            {
+                ascii = ".";
+            }
+            else if (value >= 0.70)
+            {
+                ascii = "*";
+            }
+            else if (value >= 0.60)
+            {
+                ascii = ":";
+            }
+            else if (value >= 0.50)
+            {
+                ascii = "&";
+            }
+            else if (value >= 0.40)
+            {
+                ascii = "8";
+            }
+            else if (value >= 0.30)
+            {
+                ascii = "#";
+            }
+            else
+            {
+                ascii = "@";
+            }
+            return ascii;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(saveFile.FileName);
+                writer.Write(richTextBox1.Text);
+                saveFile.Filter = "Text Files|*.txt"; 
+                writer.Close();
+
+            }
+        }
+    }
+}
